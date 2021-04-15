4f18ae679b3b5ae216faa31c651b99e7300c4641
majolagos <mlagos.r90@gmail.com>
2021-04-15 11:04:37 -0400

    create element

---
+++ src/views/login.js
@@ -0,0 +1,28 @@
+//header
+
+const login = () => {
+
+    const headerLogin = document.createElement('header');
+    headerLogin.className = "headerLogin";
+    const divTest = document.createElement('div');
+    headerLogin.appendChild('divTest');
+    
+    /*const divCollapse = document.createElement('div');
+    divCollapse.className = "collapse";
+    divCollapse.id = "navbarToggleExternalContent";
+    const divBrand = document.createElement('div');
+    divBrand.className = "bg-dark p-4";
+    */
+
+
+     document.getElementsByClassName('headerLogin').appendChild(document.createElement('div'));
+   
+
+
+    //login
+    //footer del login
+
+}
+
+
+export default login
