b2aaaec122520bffdb9cdac0e955cbaf9ebb1848
majolagos <mlagos.r90@gmail.com>
2021-04-19 09:09:31 -0400

    module archives navigation bar and routes

---
+++ src/components/routes.js
@@ -0,0 +1,34 @@
+
+    const routes =()=>{    
+    const inicio = document.getElementById("Inicio");
+    const cursos = document.getElementById("Cursos");
+    const servicios = document.getElementById("Servicios");
+    const puntosLimpios = document.getElementById("Puntos Limpios");
+
+
+// inicio.addEventListener('click', e =>{
+//     history.pushState(null, "Inicio", "/inicio");
+   
+// });
+
+// cursos.addEventListener('click', e =>{
+//     history.pushState(null, "Cursos", "/cursos");
+    
+// });
+
+// servicios.addEventListener('click', e =>{
+//     history.pushState(null, "Servicios", "/servicios");
+     
+// });
+
+// puntosLimpios.addEventListener('click', e =>{
+//     history.pushState(null, "Puntos Limpios", "/puntos-limpios");
+     
+// });
+
+
+}
+
+
+
+export default routes
