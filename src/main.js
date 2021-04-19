080e2ee79457f94ac34dc1fb57ccb59ea56b37b4
ccnarvaez <ccnarvaez@gmail.com>
2021-04-19 16:32:39 -0400

    Mary Kondo archive

--- src/main.js
+++ src/main.js
@@ -1,46 +1,14 @@
-// Este es el punto de entrada de tu aplicacion
-// identificar la url en la que estoy y cargar el nav que corresponda... usar nav como un componente aparte
-//aca se arma la vista dinamica!
-
-import {
-    myFunction
-} from './components/index.js';
-
-// import config from './components/config.js';
-import login from './views/login.js';
-// import login2 from './views/login2.js';
-// import firebaseConfig from './components/config.js';
-// import firebase from '/firebase'
-//  firebase.initializeApp(firebaseConfig);
-// import login from './views/login.js';
 
 import register from './components/register.js'
 import routes from './components/routes.js'
 import navbar from './views/navbar.js';
-import login from './views/login.js';
+import barTemplate from './views/template.js';
 
+// Firebase callback function (register)
 const btnLogin = document.getElementById('btnLogin');
 btnLogin.addEventListener('click', ()=>{
-    
-
 const test = register();
-console.log(test); 
 });
-
-
-//identificar la ruta... dependiendo de eso cargo la vista
-
-const header = document.querySelector('header');
-const main = document.querySelector('main');
-const nav = navbar;
-header.appendChild(nav)
-
-const prueba = document.createElement('h1');
-prueba.textContent = 'Hola!';
-
-// main.appendChild(login2);
-//si el estado o path de la url es inicio... cargo la vista de inicio... etc.
-//url = path === '/' ? inicio : pathname.replace('/','')
 
 window.addEventListener('hashchange', () =>{
 
@@ -70,8 +38,6 @@ switch (window.location.hash){
 
 });
 
-
-
-myFunction();
-// login();
+// Callback function
 routes();
+barTemplate();
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,6 @@
 // importamos la funcion que vamos a testear
 import login from '../src/views/login';
+import register from './components/register.js'
 
 describe('login', () => {
   it('debería ser una función', () => {
@@ -10,5 +11,17 @@ describe('login', () => {
 describe('login render', () => {
   it('should render without crashing', () => {
     expect(login).not.toThrow('error');
+  });
+});
+
+describe('register', () => {
+  it('debería ser una función', () => {
+    expect(typeof login).toBe('function');
+  });
+});
+
+describe('register render', () => {
+  it('should render without crashing', () => {
+    expect(register).not.toThrow('error');
   });
 });
