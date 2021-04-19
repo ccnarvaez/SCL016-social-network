fb63606a0d6c25018943bc0f016f7f3ab09b3f14
ccnarvaez <ccnarvaez@gmail.com>
2021-04-19 15:35:05 -0400

    Firebase off

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-// aqui exportaras las funciones que necesites
-//firebase
-
-export const myFunction = () => {
-  // aqui tu codigo
-  // console.log('Hola mundo!');
-};
--- src/components/register.js
+++ src/components/register.js
@@ -1,27 +1,32 @@
-
 const register = () => {
+  // Getting user data
   const email = document.getElementById('txtEmail').value;
   const password = document.getElementById('txtPassword').value;
+
+  // Creating event
   const loginForm = document.getElementById('loginForm');
-  loginForm.addEventListener('submit', (e)=>{
-  e.preventDefault();
+  loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
     });
   
+  // This is to send data from app to firebase
   auth.createUserWithEmailAndPassword(email, password)
   .then(userCredential => {
-    // Signed in
+    // Sign in
     let user = userCredential.user;
+    console.log(user);
+    console.log(userCredential);
     loginForm.reset();
-    // ...
   })
   .catch((error) => {
+    // Bad login messages
     let errorCode = error.code;
     let errorMessage = error.message;
-   console.log(errorCode);
-   console.log(errorMessage);
+    console.log(errorCode);
+    console.log(errorMessage);
   });
 
-  return true //true or false
+  return true 
 
 }
 
